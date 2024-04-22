3af4d15d3fd275ec0e191ac0b7030cdb14a3d098
Pieter Uys <pieter@openvantage.co.za>
2024-04-23 00:15:21 +0200

    WIP: busy with addressing the android studio PATH issue

--- utils/android_utils.py
+++ utils/android_utils.py
@@ -1,38 +1,49 @@
+import os
 import subprocess
 import sys
+import platform
 import logging
 from colorama import Fore, Style
 
 def check_android_studio_installation():
+    os_type = platform.system()
     try:
-        android_studio_version = subprocess.check_output(["studio.bat", "--version"], shell=True).decode().strip()
-        print(f"{Fore.GREEN}Android Studio version: {android_studio_version}{Style.RESET_ALL}")
-        logging.info(f"Android Studio version: {android_studio_version}")
+        if os_type == "Windows":
+            # Attempt to check the version using 'studio.bat' on Windows
+            android_studio_version = subprocess.check_output(["studio.bat", "--version"], shell=True).decode().strip()
+            print(f"{Fore.GREEN}Android Studio version: {android_studio_version}{Style.RESET_ALL}")
+            logging.info(f"Android Studio version: {android_studio_version}")
+        else:
+            # For macOS and other systems, check for the presence of the application
+            if os.path.exists("/Applications/Android Studio.app"):
+                print(f"{Fore.GREEN}Android Studio is installed on macOS.{Style.RESET_ALL}")
+                logging.info("Android Studio is installed on macOS.")
+            else:
+                raise FileNotFoundError
     except (FileNotFoundError, subprocess.CalledProcessError):
-        print(f"{Fore.RED}Error: Android Studio is not installed or not found in the system's PATH.{Style.RESET_ALL}")
-        print(f"{Fore.YELLOW}Please install Android Studio and ensure that the 'studio.bat' executable is available in the system's PATH.{Style.RESET_ALL}")
-        logging.error("Android Studio is not installed or not found in the system's PATH.")
+        print(f"{Fore.RED}Error: Android Studio is not installed or not found.{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}Please install Android Studio. Visit https://developer.android.com/studio/install{Style.RESET_ALL}")
+        logging.error("Android Studio is not installed or not found.")
         sys.exit(1)
 
 def check_adb_devices():
     try:
-        adb_devices_output = subprocess.check_output(["adb", "devices"]).decode().strip()
+        adb_devices_output = subprocess.check_output(["adb", "devices"], universal_newlines=True).decode().strip()
         if "List of devices attached" in adb_devices_output:
             devices = adb_devices_output.split("\n")[1:]
-            if len(devices) > 0:
+            if any(device.strip() for device in devices):
                 print(f"{Fore.GREEN}ADB devices are running:{Style.RESET_ALL}")
                 for device in devices:
                     print(device)
                 logging.info("ADB devices are running.")
             else:
-                print(f"{Fore.RED}No ADB devices found. Please start an emulator or connect a physical device.{Style.RESET_ALL}")
+                print(f"{Fore.RED}No ADB devices found. Starting an emulator...{Style.RESET_ALL}")
                 logging.error("No ADB devices found.")
-                sys.exit(1)
         else:
             print(f"{Fore.RED}Error: Failed to retrieve ADB devices.{Style.RESET_ALL}")
             logging.error("Failed to retrieve ADB devices.")
             sys.exit(1)
-    except (FileNotFoundError, subprocess.CalledProcessError):
+    except FileNotFoundError:
         print(f"{Fore.RED}Error: ADB is not installed or not found in the system's PATH.{Style.RESET_ALL}")
         print(f"{Fore.YELLOW}Please ensure that you have the Android Debug Bridge (adb) installed and available in the system's PATH.{Style.RESET_ALL}")
         logging.error("ADB is not installed or not found in the system's PATH.")
@@ -40,13 +51,17 @@ def check_adb_devices():
 
 def get_android_device_info():
     try:
-        device_info = subprocess.check_output(["adb", "shell", "getprop"]).decode().strip().split("\n")
-        platform_version = next(line.split(": ")[1] for line in device_info if line.startswith("ro.build.version.release"))
-        device_name = next(line.split(": ")[1] for line in device_info if line.startswith("ro.product.model"))
+        device_info = subprocess.check_output(["adb", "shell", "getprop"], universal_newlines=True).strip().split("\n")
+        platform_version = next((line.split(": ")[1].strip() for line in device_info if "ro.build.version.release" in line), None)
+        device_name = next((line.split(": ")[1].strip() for line in device_info if "ro.product.model" in line), None)
         automation_name = "UIAutomator2"
+
+        if not platform_version or not device_name:
+            raise ValueError("Device information incomplete.")
+
         return platform_version, device_name, automation_name
-    except subprocess.CalledProcessError:
-        print(f"{Fore.RED}Error: Failed to retrieve device information.{Style.RESET_ALL}")
+    except (subprocess.CalledProcessError, ValueError) as e:
+        print(f"{Fore.RED}Error: Failed to retrieve device information. {e}{Style.RESET_ALL}")
         print(f"{Fore.YELLOW}Please ensure that you have the Android Debug Bridge (adb) installed and available in the system's PATH.{Style.RESET_ALL}")
         logging.error("Failed to retrieve device information.")
         sys.exit(1)
--- utils/appium_utils.py
+++ utils/appium_utils.py
@@ -1,29 +1,55 @@
+import socket
 import subprocess
 import sys
 import logging
 import platform
 from colorama import Fore, Style
-import os
 
-print("PATH !!!!", os.environ['PATH'])
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
 
 def check_appium_installation():
     try:
-        appium_version = subprocess.check_output(["appium", "--version"]).decode().strip()
+        # Detect the operating system
+        os_type = platform.system()
+        # Use 'appium.cmd' for Windows and 'appium' for others
+        appium_command = "appium.cmd" if os_type == "Windows" else "appium"
+
+        # Get the Appium version
+        appium_version = subprocess.check_output([appium_command, "--version"], universal_newlines=True).strip()
         print(f"{Fore.GREEN}Appium version: {appium_version}{Style.RESET_ALL}")
         logging.info(f"Appium version: {appium_version}")
-    except (FileNotFoundError, subprocess.CalledProcessError):
+    except (FileNotFoundError, subprocess.CalledProcessError) as e:
         print(f"{Fore.RED}Error: Appium is not installed or not found in the system's PATH.{Style.RESET_ALL}")
         print(f"{Fore.YELLOW}Please install Appium and ensure that the Appium executable is available in the system's PATH.{Style.RESET_ALL}")
-        logging.error("Appium is not installed or not found in the system's PATH.")
+        logging.error(f"Appium is not installed or not found in the system's PATH: {e}")
         sys.exit(1)
 
 def start_appium_server():
+    appium_port = 4723  # default Appium port
+    if is_port_in_use(appium_port):
+        print(f"{Fore.YELLOW}Appium server already running on port {appium_port}{Style.RESET_ALL}")
+        logging.info(f"Appium server already running on port {appium_port}")
+        return
+    
     try:
-        appium_path = subprocess.check_output(["where" if platform.system() == "Windows" else "which", "appium"]).decode().strip().splitlines()[0]
-    except subprocess.CalledProcessError:
-        print(f"{Fore.RED}Error: Failed to find the Appium executable path.{Style.RESET_ALL}")
-        logging.error("Failed to find the Appium executable path.")
+        # Construct the command to start Appium in a new terminal
+        os_type = platform.system()
+        if os_type == "Windows":
+            appium_command = "start cmd /c appium"
+        else:  # macOS, Linux, and other Unix-like systems
+            appium_command = "osascript -e 'tell app \"Terminal\" to do script \"appium\"'"
+        
+        # Execute the command to start Appium in a new terminal
+        subprocess.Popen(appium_command, shell=True)
+        print(f"{Fore.BLUE}Appium server should now be starting in a new terminal window...{Style.RESET_ALL}")
+        logging.info("Appium server command executed to open in a new terminal.")
+
+    except Exception as e:
+        print(f"{Fore.RED}An unexpected error occurred while trying to start Appium server: {e}{Style.RESET_ALL}")
+        logging.error(f"An unexpected error occurred while trying to start Appium server: {e}")
         sys.exit(1)
-    print(f"{Fore.BLUE}Starting Appium server...{Style.RESET_ALL}")
-    appium_process = subprocess.Popen([appium_path])
+
